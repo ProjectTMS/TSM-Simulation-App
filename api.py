2618ae65df92cfec434d5a49de7a96f7d3b6b992
ProjectTMS <projtms@gmail.com>
2024-10-27 13:26:20 +0600

    Update api.py 10/27 0.1

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ import os
 from threading import Event
 
 # Load the YOLO model
-model = YOLO('best.pt')
+model = YOLO('bestV2.pt')
 
 # Define the class names
 class_names = ['bicycle', 'bus', 'car', 'motorbike', 'person']
